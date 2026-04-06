2df690379565b89d6d0b8e3b57a207a22e9f85d0
IshinoTento <tento1209@icloud.com>
2026-04-06 15:57:08 +0900

    Initial Commit

---
+++ SampleTest/SampleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTestApp.swift
+//  SampleTest
+//
+//  Created by 石野天斗 on 2026/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
