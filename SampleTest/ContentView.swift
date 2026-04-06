b5899f4526760e466fb4c3b5e79cb0745d70bacf
IshinoTento <tento1209@icloud.com>
2026-04-06 16:17:06 +0900

    Textを追加しました。

--- SampleTest/ContentView.swift
+++ SampleTest/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("branch4です．")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
